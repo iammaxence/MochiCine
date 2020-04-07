f0e81156f60d52d89a05e29cd3c6352c9e494823
iammaxence <maxence-brunet@hotmail.fr>
2020-04-07 15:41:32 +0200

    First step

---
+++ src/database/DBStatic.java
@@ -0,0 +1,18 @@
+package database;
+
+
+public class DBStatic {
+	public static String mysql_host="localhost";
+	public static String mysql_db="Brunet_Lin"; //changer ici
+	public static String mysql_username="root";
+	public static String mysql_password="root";
+	public static boolean mysql_pooling=true; 
+//	public static boolean mysql_pooling=false; 
+	//EXPORT -> mettre a true
+	//pooling true -> on utilise Datasource qui fait la connection automatique donc pas local
+	//pooling false -> connection locale avec DBStatic
+	
+	public static String mongodb_host="mongodb://localhost:27017";
+	public static String mongodb_port="27017"; //changer ici maybe
+	public static String mongodb_db="Brunet_Lin"; //changer ici
+}
