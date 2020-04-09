5fe2d9d4b045d60ad126b5f9ece5c291998be82f
Laetitia PHAM <47776848+laetiitia@users.noreply.github.com>
2020-04-09 12:52:25 +0200

    update

--- src/services/Enregistrement.java
+++ src/services/Enregistrement.java
@@ -2,11 +2,24 @@ package services;
 
 import org.json.JSONObject;
 
+import tools.ErrorJSON;
+import tools.CheckTools;
+
+
+
 public class Enregistrement {
 	
 	
-	public static JSONObject register(String login,String mdp) {
-		return null;
+	public static JSONObject createUser(String login, String mdp) {
+		if(login.equals("") || mdp.equals("")) {
+			return ErrorJSON.serviceRefused("Register: Argument Null", -1);
+		}
+		
+		if(CheckTools.checkUser(login)) {
+			return ErrorJSON.serviceRefused("Register: Pseudo "+login+" already existe", -2);
+		}
+		
+		return CheckTools.insertUser(login, mdp);
 		
 	}
 }
